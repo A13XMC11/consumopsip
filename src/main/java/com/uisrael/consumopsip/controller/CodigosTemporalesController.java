888bfd0c2b65593e4f2b6158683144667f40f17a
GebhardLeonardo-collab <jarrinleonardo009@gmail.com>
2026-07-13 11:02:12 -0700

    feat: crear vistas HTML de CodigoTemporal, EmpleadoHorario y Horarios

--- src/main/java/com/uisrael/consumopsip/controller/CodigosTemporalesController.java
+++ src/main/java/com/uisrael/consumopsip/controller/CodigosTemporalesController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/codigo")
+@RequestMapping("/codigostemporales")
 public class CodigosTemporalesController {
 	
 	@GetMapping
 	public String leerPagina() {
-		return "codigo/listarcodigo";
+		return "codigostemporales/listarcodigostemporales";
 	}
 }
