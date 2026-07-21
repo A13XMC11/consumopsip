593d9318ebfc6b03213cb2a5a3ee93bb071666db
GebhardLeonardo-collab <jarrinleonardo009@gmail.com>
2026-07-20 22:11:36 -0700

    update: cambios en empleadoHorarioController

--- src/main/java/com/uisrael/consumopsip/controller/EmpleadoHorarioController.java
+++ src/main/java/com/uisrael/consumopsip/controller/EmpleadoHorarioController.java
@@ -10,14 +10,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.uisrael.consumopsip.model.dto.request.EmpleadoHorarioRequestDto;
 import com.uisrael.consumopsip.model.dto.response.EmpleadoHorarioResponseDto;
+import com.uisrael.consumopsip.model.dto.response.EmpleadoResponseDto;
+import com.uisrael.consumopsip.model.dto.response.HorariosResponseDto;
 import com.uisrael.consumopsip.service.IEmpleadoHorarioService;
+import com.uisrael.consumopsip.service.IEmpleadoService;
+import com.uisrael.consumopsip.service.IHorariosService;
 
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/empleadohorario")
 public class EmpleadoHorarioController {
+	
+    @Autowired
+    private IEmpleadoService servicioEmpleado;
 
+    @Autowired
+    private IHorariosService servicioHorarios;
+    
     @Autowired
     private IEmpleadoHorarioService servicioEmpleadoHorario;
 
@@ -34,9 +44,14 @@ public class EmpleadoHorarioController {
 
     @GetMapping("/nuevo")
     public String nuevoEmpleadoHorario(HttpSession session, Model model) {
-        if (session.getAttribute("token") == null) {
+    	String token = (String) session.getAttribute("token");
+        if (token == null) {
             return "redirect:/login";
         }
+        List<EmpleadoResponseDto> empleadosBD = servicioEmpleado.listarEmpleados(token);
+        List<HorariosResponseDto> horariosBD = servicioHorarios.listarHorarios(token);
+        model.addAttribute("listaempleados", empleadosBD);
+        model.addAttribute("listahorarios", horariosBD);
         model.addAttribute("empleadohorario", new EmpleadoHorarioRequestDto());
         return "empleadohorario/crearempleadohorario";
     }
